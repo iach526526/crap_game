c7732039137a37bfba5518d2370313efc15eafd2
Each Chen <iach526526@gmail.com>
2022-06-03 10:51:14 +0800

    更新_重複遊玩
    可以決定要玩幾次

--- Crap 遊戲.cpp
+++ Crap 遊戲.cpp
@@ -2,42 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 int throw_dice();
+ int round_1(int total);
 int main()
 {
-	int total,round,target;
+	int total,round,target,result[99],flag=1,i;
 	srand((unsigned)(time(0)));
-	total=throw_dice();
 	
+	printf("想玩幾次呢？");
+	scanf("%d",&round);
 	//第一次投出7or11算贏;2、3、12算輸;其餘設為之後目標 
-	if (total==7||total==11)
+	
+	for(i=0;i<round;i++)
 	{
-		printf("你贏了");
-		return 0;//結束 
-	}
-	else if(total==2||total==3||total==12)
-	{
-		printf("你輸了");
-		return 0;//結束 
-	}
-	else
-	{
-		target=total;
+		printf("\n第%d局\n",i+1);
+		target=round_1(throw_dice());
+		if (target==1)
+		{
+			continue;//換下一局 
+		}
+		else
+		{
+			do
+			{
+				total=throw_dice();
+			}while(total!=7 and total!=target);//一直重複直到擲出 7or target 
+			
+			if (total==7)
+			{
+				printf("你輸了\n");
+				continue;//結束 
+			}
+			else
+			{
+				printf("你贏了\n");
+				continue;//結束 
+			} 
+		}
 	}
 	
-	do
-	{
-		total=throw_dice();
-	}while(total!=7 and total!=target);//一直重複直到擲出 7or target 
-	if (total==7)
-	{
-		printf("你輸了");
-		return 0;//結束 
-	}
-	else
-	{
-		printf("你贏了");
-		return 0;//結束 
-	} 
+	
 	
  } 
  int throw_dice()//做擲骰子的動作 
@@ -50,4 +53,25 @@ int main()
 	//printf("擲出了%d%+d\n",dice1,dice2);//後台除錯用，沒有非常必要知道每次擲出什麼 
 	printf("共擲出了%d\n",total);
 	return total;
+ }
+ int round_1(int total)
+ {
+ 	int flag;
+	if (total==7||total==11)
+	{
+		printf("你贏了\n");
+		flag=1;//結束 
+		return flag;
+	}
+	else if(total==2||total==3||total==12)
+	{
+		printf("你輸了\n");
+		flag=1;//結束 
+		return flag;
+	}
+	else
+	{
+		flag=total;
+		return flag;
+	}
  }
